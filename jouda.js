c1cf1efb8fe1de62d439e30767337790cc4180fb
Tegar Ibrahim <0259230052@ioh.co.id>
2025-08-21 08:35:03 +0700

    innitial commit

---
+++ jouda.js
@@ -0,0 +1,24 @@
+//Mobile Menu
+
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId);
+    const nav = document.getElementById(navId);
+
+    if(toggle && nav){ toggle.addEventListener('click', () => { nav.classList.toggle('show'); }) } 
+}
+
+showMenu('nav-toggle','nav-menu');
+
+// Toggling Mobile Menu when a navlink is clicked
+
+const navLink = document.querySelectorAll('.nav-link');
+
+function linkAction() {
+    navLink.forEach(n => n.classList.remove('active'))
+    this.classList.add('active');
+
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show');
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction));
